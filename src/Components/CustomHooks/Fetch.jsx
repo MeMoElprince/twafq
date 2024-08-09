83c7c017f9cd11a6001409cc2ab9d2301e6833d4
IMohammedNasr <mohnasr2002@gmail.com>
2024-08-09 14:42:50 +0300

    x

--- src/Components/CustomHooks/Fetch.jsx
+++ src/Components/CustomHooks/Fetch.jsx
@@ -10,11 +10,11 @@ export default async function Fetch({ url, setData, setLoading, setErrorMessage,
     })
     const string = await response.text();
     const data = string === "" ? {} : JSON.parse(string);
+    console.log(data);
     const Status = response.status;
     if (Status >= 200 && Status < 300) {
       data.status = 'success'
     }
-    console.log(data.message);
     setData(data)
     if (data.status !== 'success') {
       throw new Error(data.message)
